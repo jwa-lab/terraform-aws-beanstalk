3dd9098f168d3a1bd31a95cc82444262f62c861d
Thomas Klein <thomas.klein@jwalab.com>
2023-01-09 03:24:58 +0100

    added output for instance role id

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
 output "instances_security_group_id" {
   value = aws_security_group.beanstalk_instances_security_group.id
 }
+
+output "instance_role_id" {
+  value = aws_iam_role.beanstalk_instances_role.id
+}
