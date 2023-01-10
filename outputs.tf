d795cc6086efd835c74edc562fc82e0add934219
Thomas Klein <thomas.klein@jwalab.com>
2023-01-10 09:18:03 +0100

    add load balancer arn output

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,7 @@ output "instances_security_group_id" {
 output "instance_role_id" {
   value = aws_iam_role.beanstalk_instances_role.id
 }
+
+output "load_balancer_arn" {
+  value = aws_elastic_beanstalk_environment.beanstalk_env.load_balancers[0]
+}
