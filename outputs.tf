70a2043c0fcd59321a441783322fac4f2de88db2
Thomas Klein <thomas.klein@jwalab.com>
2023-01-10 09:29:48 +0100

    added beanstalk env cname to outputs

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "beanstalk_env_cname" {
+  value = aws_elastic_beanstalk_environment.beanstalk_env.cname
+}
+
 output "instances_security_group_id" {
   value = aws_security_group.beanstalk_instances_security_group.id
 }
