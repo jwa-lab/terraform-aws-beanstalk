4ce36cb757f28de02a43841440444ab68d04d93d
Thomas Klein <thomas.klein@jwalab.com>
2023-03-27 12:25:52 +0200

    Output the full role

--- outputs.tf
+++ outputs.tf
@@ -6,8 +6,8 @@ output "instances_security_group_id" {
   value = aws_security_group.beanstalk_instances_security_group.id
 }
 
-output "instance_role_id" {
-  value = aws_iam_role.beanstalk_instances_role.id
+output "instance_role" {
+  value = aws_iam_role.beanstalk_instances_role
 }
 
 output "load_balancer_arn" {
